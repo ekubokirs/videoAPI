1831cdfad20c2954ee729aade3ff18736aef54c0
Emilia Kirschenbaum <ekubokirs@gmail.com>
2013-12-03 16:26:00 -0800

    created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoAPI::Application.config.secret_key_base = '2f7c6f5bc36d205c92383371ed44d70b2c6fa0177a7a908375977f20a55debf2fb5ba1f7dc78274485c346ff705eda587b168fae69c186093c816a74fa640aa4'
